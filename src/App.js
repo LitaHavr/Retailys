825be10770488ecdd363cd61d3a1d601103b0509
LitaHavr <88926310+LitaHavr@users.noreply.github.com>
2022-03-16 17:12:05 +0100

    Retailys

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import {XMLParser, XMLBuilder} from "fast-xml-parser";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoading: true,
+            data: null,
+            names: [],
+            categoryNames: []
+        };
+    }
+
+    async componentDidMount() {
+        axios
+            .get("./export_full.xml", {
+                "Content-Type": "application/xml; charset=utf-8",
+            })
+            .then((response) => {
+                const parser = new XMLParser({
+                    ignoreAttributes: false,
+                    attributeNamePrefix: '',
+                    attributesGroupName: '',
+                    allowBooleanAttributes: true,
+                });
+                const jsonObj = parser.parse(response.data);
+
+                const allItems = jsonObj.export_full.items.item
+                const allCategory = jsonObj.export_full.categoriesWithParts.category
+                const categoryNames = allCategory.map(item => item.name)
+                const names = allItems.map(item => item.name)
+                const parts = allItems.map(item => item.export)
+                this.setState({data: jsonObj.export_full, isLoading: false, names: names, categoryNames: categoryNames})
+
+            })
+            .catch((error) => {
+                this.setState({isLoading: false})
+                console.log(error);
+            });
+
+    }
+
+    render() {
+
+        const listItem = this.state.names.map((name,i) =>
+            <li key={i}>{name}</li>
+        )
+        const listItemCategories = this.state.categoryNames.map((name,i) =>
+            <li key={i}>{name}</li>
+        )
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <div className='left'>
+                        <h3>I could not complete step 3, but you have a list of all categories 	&#129303; &#9654;</h3>
+                        <h2> All items:{this.state.isLoading ? 'Loading...' : this.state.data.items.item.length}</h2>
+                        <ol>
+                            {listItem}
+                        </ol>
+                    </div>
+
+                    <div className='right'>
+                    <h2>All categories : {this.state.isLoading ? 'Loading...' : this.state.categoryNames.length}</h2>
+                    <ol>
+                        {listItemCategories}
+                    </ol>
+                    </div>
+
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
